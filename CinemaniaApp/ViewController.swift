393fd083e046c1b3883d6c1cad7f0697c2807dfd
Toygun Çil <toyguncil1@gmail.com>
2023-12-07 20:16:40 +0300

    Initial Commit

---
+++ CinemaniaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CinemaniaApp
+//
+//  Created by Toygun Çil on 7.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
